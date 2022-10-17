f0872f7b8a10d1e272ca8487109d2a6c805b983c
tatsianadev <tatsianadev@outlook.com>
2022-10-17 16:58:55 +0200

    Identity. Login/Logout added

--- FitMeApp/Controllers/AccountController.cs
+++ FitMeApp/Controllers/AccountController.cs
@@ -58,8 +58,58 @@ namespace FitMeApp.Controllers
             catch (Exception e)
             {
                 throw e;
+            }           
+        }
+
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel() { ReturnUrl = returnUrl });
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(model.Name, model.Password, model.RememberMe, false);
+                    if (result.Succeeded)
+                    {
+                        if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                        {
+                            return Redirect(model.ReturnUrl);
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Invalid password and(or) login");
+                    }
+                }
+                return View(model);
+
             }
-           
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
