2aa23bdccb11e2c5e5db6d9536875e8e3c3ab5e0
tatsianadev <tatsianadev@outlook.com>
2022-10-17 17:34:32 +0200

    Identity. Get Users list added

---
+++ FitMeApp/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using FitMeApp.Contracts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitMeApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UsersController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        public IActionResult UsersList()
+        {
+            var users = _userManager.Users.ToList();
+            return View(users);
+        }
+    }
+}
