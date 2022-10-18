26abe19d343e5284c66d87d5b98bb0d5c6b6d420
tatsianadev <tatsianadev@outlook.com>
2022-10-18 14:16:46 +0200

    Identity. CreateUser() added.

--- FitMeApp/Controllers/UsersController.cs
+++ FitMeApp/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using FitMeApp.Contracts;
+using FitMeApp.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -22,6 +23,49 @@ namespace FitMeApp.Controllers
         {
             var users = _userManager.Users.ToList();
             return View(users);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    User user = new User()
+                    {
+                        UserName = model.Name,
+                        Email = model.Email,
+                        PhoneNumber = model.PhoneNumber,
+                        Year = model.Year,
+                        Gender = model.Gender
+                    };
+
+                    var result = await _userManager.CreateAsync(user, model.Password);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("UsersList");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+                return View();
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
         }
     }
 }
---
+++ FitMeApp/Models/CreateUserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitMeApp.Models
+{
+    public class CreateUserViewModel
+    {      
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }       
+        public string PhoneNumber { get; set; }        
+        public int Year { get; set; }
+        public string Gender { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        
+        
+    }
+}
