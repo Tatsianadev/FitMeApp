9ff4fdc8e38b4c72e73f91dc34aa01119a4b2f0c
tatsianadev <tatsianadev@outlook.com>
2022-11-24 17:21:34 +0100

    Schedule in progress. Before changing  passed into ScheduleView model

---
+++ FitMeApp.WEB.Contracts/ViewModels/CurrentDayEventsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitMeApp.WEB.Contracts.ViewModels
+{
+    public class CurrentDayEventsViewModel
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public string MonthName { get; set; }
+        public int  Day { get; set; }
+        public string DayName { get; set; }
+        public ICollection<EventViewModel> Events { get; set; }
+    }
+}
