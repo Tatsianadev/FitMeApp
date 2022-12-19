7c59b8738f08f412b4fd109c33d3b50334710dec
tatsianadev <tatsianadev@outlook.com>
2022-12-19 09:15:39 +0100

    CalendarView improving in progress. Part 1
    repetitive parts of code are moved to viewComponent. Code doesn't work (in progress)

---
+++ FitMeApp/ViewComponents/DateOnCalendarViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using FitMeApp.WEB.Contracts.ViewModels;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitMeApp.ViewComponents
+{
+    public class DateOnCalendarViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke(CurrentDayEventsViewModel model)
+        {
+            
+            return View("DateOnCalendar", model);
+        }
+    }
+}
