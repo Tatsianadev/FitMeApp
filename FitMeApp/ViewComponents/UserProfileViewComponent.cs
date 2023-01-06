5d2eb294dbc9628cc08f127d3d5d2b26bed8b340
tatsianadev <tatsianadev@outlook.com>
2023-01-06 14:22:15 +0100

    UserProfileViewComponent is added (with bages of notifications)
    There is need to add logic in viewComponent.

---
+++ FitMeApp/ViewComponents/UserProfileViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using FitMeApp.Common;
+using FitMeApp.Services.Contracts.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitMeApp.ViewComponents
+{
+    public class UserProfileViewComponent: ViewComponent
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IFitMeService _fitMeService;
+
+        public UserProfileViewComponent(UserManager<User> userManager, IFitMeService fitMeService)
+        {
+            _userManager = userManager;
+            _fitMeService = fitMeService;
+        }
+
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.ChangedEventsCount = 2; //test
+            ViewBag.NotificationCount = 2; //test
+            return View("UserProfile");
+        }
+    }
+}
