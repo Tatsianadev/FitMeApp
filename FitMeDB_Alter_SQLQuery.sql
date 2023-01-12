aa01871ec6fc4ca086e27b7008fda312804541f6
tatsianadev <tatsianadev@outlook.com>
2023-01-12 16:13:41 +0100

    Db. Insert  Users, Roles, UserRole tables

--- FitMeDB_Alter_SQLQuery.sql
+++ FitMeDB_Alter_SQLQuery.sql
@@ -1,44 +1,44 @@
 USE [FitMeDB]
 GO
 
-alter table AspNetUsers
-add 
-FirstName nvarchar (256),
-LastName nvarchar (256)
-go
+--alter table AspNetUsers
+--add 
+--FirstName nvarchar (256),
+--LastName nvarchar (256)
+--go
 
 
-alter table AspNetUsers
-add 
-LastName nvarchar (256)
-go
+--alter table AspNetUsers
+--add 
+--LastName nvarchar (256)
+--go
 
-alter table AspNetUsers
-add 
-Avatar nvarchar (256)
-go
+--alter table AspNetUsers
+--add 
+--Avatar nvarchar (256)
+--go
 
 
-alter table Trainers
-drop column FirstName
-go
+--alter table Trainers
+--drop column FirstName
+--go
 
-alter table Trainers
-drop column LastName
-go
+--alter table Trainers
+--drop column LastName
+--go
 
 alter table Trainers
 drop constraint [DF__Trainers__Pictur__1B9317B3]
 go
 
-alter table Trainers
-drop column Picture
-go
+--alter table Trainers
+--drop column Picture
+--go
 
-alter table Trainers
-drop column Gender
-go
+--alter table Trainers
+--drop column Gender
+--go
 
-alter table Trainers
-add Status int
-go
+--alter table Trainers
+--add Status int
+--go
