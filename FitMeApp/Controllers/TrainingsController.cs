8fbe0dbc43194f35bc8c917c8aa55b942c545afa
DESKTOP-9FNNS2M\tatsiana <tatsianadev@outlook.com>
2023-02-08 10:08:35 +0100

    ApplyingTraining logic in progress. Part 6. Input Id validation added

--- FitMeApp/Controllers/TrainingsController.cs
+++ FitMeApp/Controllers/TrainingsController.cs
@@ -9,6 +9,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using FitMeApp.WEB.Contracts.ViewModels;
 using FitMeApp.Common;
+using Microsoft.AspNetCore.Identity;
 
 namespace FitMeApp.Controllers
 {
@@ -16,13 +17,15 @@ namespace FitMeApp.Controllers
     {
         private readonly ITrainingService _trainingService;
         private readonly IFitMeService _fitMeService;
+        private readonly UserManager<User> _userManager;
         private readonly ModelViewModelMapper _mapper;
         private readonly ILogger _logger;
 
-        public TrainingsController(ITrainingService trainingService, IFitMeService fitMeService, ILogger<TrainersController> logger)
+        public TrainingsController(ITrainingService trainingService, IFitMeService fitMeService,UserManager<User> userManager, ILogger<TrainersController> logger)
         {
             _trainingService = trainingService;
             _fitMeService = fitMeService;
+            _userManager = userManager;
             _mapper = new ModelViewModelMapper();
             _logger = logger;
         }
@@ -35,8 +38,14 @@ namespace FitMeApp.Controllers
 
 
 
-        public IActionResult ApplyForPersonalTraining(string trainerId)
+        public async Task<IActionResult> ApplyForPersonalTraining(string trainerId)
         {
+            var user = await _userManager.GetUserAsync(User);
+            if (trainerId == user.Id || string.IsNullOrEmpty(trainerId))
+            {
+                return RedirectToAction("Index", "Trainers");
+            }
+
             var trainer = _fitMeService.GetTrainerWithGymAndTrainings(trainerId);
             ApplyingForPersonalTrainingViewModel model = new ApplyingForPersonalTrainingViewModel()
             {
