b900314003c5e0dd875463f3112505b14dd8252c
DESKTOP-9FNNS2M\tatsiana <tatsianadev@outlook.com>
2023-03-24 20:29:54 +0100

    Work with Excel files. Part 6 (ExcelReportOpenXml in progress)

--- FitMeApp.Services/ExcelReportOpenXml.cs
+++ FitMeApp.Services/ExcelReportOpenXml.cs
@@ -4,10 +4,18 @@ using System.Data;
 using System.IO;
 using System.Text;
 using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Drawing;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using FitMeApp.Services.Contracts.Interfaces;
-
+using BottomBorder = DocumentFormat.OpenXml.Spreadsheet.BottomBorder;
+using Fill = DocumentFormat.OpenXml.Spreadsheet.Fill;
+using Fonts = DocumentFormat.OpenXml.Spreadsheet.Fonts;
+using ForegroundColor = DocumentFormat.OpenXml.Spreadsheet.ForegroundColor;
+using LeftBorder = DocumentFormat.OpenXml.Spreadsheet.LeftBorder;
+using PatternFill = DocumentFormat.OpenXml.Spreadsheet.PatternFill;
+using RightBorder = DocumentFormat.OpenXml.Spreadsheet.RightBorder;
+using TopBorder = DocumentFormat.OpenXml.Spreadsheet.TopBorder;
 
 namespace FitMeApp.Services
 {
@@ -26,6 +34,7 @@ namespace FitMeApp.Services
             }
         }
 
+
         private WorkbookPart AddWorkbookPart(SpreadsheetDocument spreadsheetDocument)
         {
             WorkbookPart workbookPart = spreadsheetDocument.AddWorkbookPart();
@@ -33,11 +42,55 @@ namespace FitMeApp.Services
             return workbookPart;
         }
 
+
         private void AddNewPartStyle(WorkbookPart workbookPart)
         {
-            WorkbookStylesPart stypePart = workbookPart.AddNewPart<WorkbookStylesPart>();
-            //stypePart.Stylesheet = GenerateStyle
+            WorkbookStylesPart stylePart = workbookPart.AddNewPart<WorkbookStylesPart>();
+            stylePart.Stylesheet = GenerateStylesSheet();
+            stylePart.Stylesheet.Save();
         }
 
+
+        private Stylesheet GenerateStylesSheet()
+        {
+            Fonts fonts = GenerateFonts();
+            Fills fills = GenerateFills();
+            Borders borders = GenerateBorders();
+            return new Stylesheet();
+        }
+
+
+        private Fonts GenerateFonts()
+        {
+            Fonts fonts = new Fonts(
+                new Font(new FontSize() {Val = 10}, new FontName() {Val = "Arial Unicode"}),
+                new Font(new FontSize() {Val = 10}, new Bold()));
+            
+            return fonts;
+        }
+        
+
+        private Fills GenerateFills()
+        {
+            Fills fills = new Fills(
+                new Fill(new PatternFill() { PatternType = PatternValues.None }),
+                new Fill(new PatternFill() { PatternType = PatternValues.Gray125 }),
+                new Fill(new PatternFill(new ForegroundColor{ Rgb = new HexBinaryValue(){Value = "66666666" } }) { PatternType = PatternValues.Solid }));
+
+            return fills;
+        }
+
+
+        private Borders GenerateBorders()
+        {
+            Borders borders = new Borders(
+                new Border(
+                    new LeftBorder(new Color(){Auto = true}){Style = BorderStyleValues.Thin}, 
+                    new RightBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+                    new TopBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+                    new BottomBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin }));
+
+            return borders;
+        }
     }
 }
