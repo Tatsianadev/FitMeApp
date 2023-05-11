4d1330c4678742caa9af926f25a11255fa4194b0
DESKTOP-9FNNS2M\tatsiana <tatsianadev@outlook.com>
2023-05-11 12:19:16 +0200

    GroupTrainings. Part 11. CurrentGroupClassScheduleView in progress.
    GroupClassesPerDayOnScheduleCalendarViewComponent (test) added.

---
+++ FitMeApp/ViewComponents/GroupClassesPerDayOnScheduleCalendarViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FitMeApp.WEB.Contracts.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitMeApp.ViewComponents
+{
+    public class GroupClassesPerDayOnScheduleCalendarViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke(List<GroupClassScheduleViewModel> grClassPerDayInfo)
+        {
+
+
+            return View("GroupClassesPerDayOnScheduleCalendar");
+        }
+    }
+}
