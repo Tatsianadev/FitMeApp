3b6f21161233cc2cdebe07278aacbafbdfc3e29d
DESKTOP-9FNNS2M\tatsiana <tatsianadev@outlook.com>
2023-06-09 19:50:38 +0200

    EnumConsistencyTest() unit test has been added.

---
+++ FitMeApp.Common/TrainingsEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitMeApp.Common
+{
+    public enum TrainingsEnum //todo write the unitTest to check if Enum accords with table in DB
+    {
+        yoga = 1,
+        pilates = 2,
+        hiit = 3,
+        wateraerobics = 4,
+        cycling = 5,
+        zumba = 6,
+        kickboxing = 7,
+        personaltraining = 8
+    }
+}
---
+++ FitMeApp.Tests/TrainingsEnumTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FitMeApp.Tests
+{
+    [TestFixture]
+    public class TrainingsEnumTests
+    {
+        [Test]
+        public void EnumConsistencyTest()
+        {
+            var trainings = CreateTestData();
+            var trainingsEnum = Enum.GetValues(typeof(Common.TrainingsEnum));
+            bool consists = false;
+
+            if (trainings.Count != trainingsEnum.Length)
+            {
+                consists = false;
+            }
+            else
+            {
+                foreach (var value in trainingsEnum)
+                {
+                    if (!trainings.ContainsKey((int)value))
+                    {
+                        consists = false;
+                        break;
+                    }
+                    else
+                    {
+                        string trainingNameToCompare = trainings[(int)value].ToLower().Replace(" ", "");
+                        if (trainingNameToCompare != value.ToString())
+                        {
+                            consists = false;
+                            break;
+                        }
+
+                        consists = true;
+                    }
+                }
+            }
+
+            Assert.IsTrue(consists);
+        }
+
+
+        private Dictionary<int, string> CreateTestData()
+        {
+            var data = new Dictionary<int, string>()
+            {
+                {1, "Yoga"},
+                {2, "Pilates"},
+                {3, "HIIT"},
+                {4, "Water Aerobics"},
+                {5, "Cycling"},
+                {6, "Zumba"},
+                {7, "Kickboxing"},
+                {8, "Personal training"},
+            };
+
+            return data;
+        }
+    }
+}
