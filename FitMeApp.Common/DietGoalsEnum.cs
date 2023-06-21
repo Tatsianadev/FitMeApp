036cf584b3dff25d72a4f3b9254588a59e119e14
DESKTOP-9FNNS2M\tatsiana <tatsianadev@outlook.com>
2023-06-21 17:16:13 +0200

    DietPlan logic in progress. Part 3.   AnthropomenricInfo view is done

--- FitMeApp.Common/DietGoalsEnum.cs
+++ FitMeApp.Common/DietGoalsEnum.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace FitMeApp.Common
 {
     public enum DietGoalsEnum
     {
+        [Description("lose weight")]
         loseWeight = 1,
+        [Description("keep weight")]
         keepWeight = 2,
+        [Description("put weight on")]
         putWeightOn = 3,
+        [Description("put muscles on")]
         putMusclesOn = 4
     }
 }
---
+++ FitMeApp.Common/EnumExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.ComponentModel;
+using System.Linq;
+
+namespace FitMeApp.Common
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum enumElement)
+        {
+            Type enumType = enumElement.GetType();
+            MemberInfo[] memberInfo = enumType.GetMember(enumElement.ToString());
+            
+            if (memberInfo != null && memberInfo.Length > 0)
+            {
+                var attributes = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+                
+                if (attributes != null && attributes.Length > 0)
+                {
+                    return ((DescriptionAttribute) attributes.ElementAt(0)).Description;
+                }
+            }
+
+            return enumElement.ToString();
+        }
+    }
+}
