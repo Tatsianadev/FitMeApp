b99accc1de581908a4fde2acf0285f62e5d59e29
DESKTOP-9FNNS2M\tatsiana <tatsianadev@outlook.com>
2023-07-07 18:12:42 +0200

    "GoBack" button on the AnthropometricInfo view (back by previous URL) is done

--- FitMeApp/Controllers/HomeController.cs
+++ FitMeApp/Controllers/HomeController.cs
@@ -1,18 +1,28 @@
-﻿using FitMeApp.Models;
+﻿using System.Collections.Generic;
+using FitMeApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 
 
 namespace FitMeApp.Controllers
 {
     public sealed class HomeController : Controller
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HomeController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
         [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
+        
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
