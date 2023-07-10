4243f58eea976ea19d38af7940dabdba938761d2
DESKTOP-9FNNS2M\tatsiana <tatsianadev@outlook.com>
2023-07-10 17:27:38 +0200

    Small fixes

--- FitMeApp.Common/NutritionsEnum.cs
+++ FitMeApp.Common/NutritionsEnum.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace FitMeApp.Common
 {
     public enum NutrientsEnum
     {
+        [Description("Proteins")]
         proteins,
+        [Description("Fats")]
         fats,
+        [Description("Carbohydrates")]
         carbohydrates
     }
 }
