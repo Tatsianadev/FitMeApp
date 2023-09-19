5e391a100d2e8a8e2caa59f31c2149d1a407e878
DESKTOP-9FNNS2M\tatsiana <tatsianadev@outlook.com>
2023-09-19 07:31:29 +0200

    Moving Chart attendence methods from GymsController to ChartController. Small fixes

--- FitMeApp/Controllers/ChartController.cs
+++ FitMeApp/Controllers/ChartController.cs
@@ -1,18 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
+using FitMeApp.Common;
+using FitMeApp.Models.ExcelModels;
+using FitMeApp.Services.Contracts.Interfaces;
 using FitMeApp.WEB.Contracts.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
-
+using System.IO;
+using System.Linq;
 
 namespace FitMeApp.Controllers
 {
     public sealed class ChartController : Controller
     {
+        private readonly IGymService _gymService;
+        private readonly ITrainerService _trainerService;
+        private readonly IFileService _fileService;
+        private readonly UserManager<User> _userManager;
         private readonly ILogger _logger;
-
-        public ChartController(ILogger<ChartController> logger)
+   
+        public ChartController(IGymService gymService, ITrainerService trainerService, IFileService fileService, UserManager<User> userManager, ILogger<ChartController> logger)
         {
+            _gymService = gymService;
+            _trainerService = trainerService;
+            _fileService = fileService;
+            _userManager = userManager;
             _logger = logger;
         }
 
@@ -30,6 +44,97 @@ namespace FitMeApp.Controllers
             }
 
             return RedirectToAction("CurrentGymInfo", "Gyms", new { gymId = gymId });
+        }
+
+
+        [Authorize(Roles = "gymAdmin")]
+        public async Task<IActionResult> LoadAttendanceChartData()
+        {
+            try
+            {
+                var user = await _userManager.GetUserAsync(User);
+                int gymId = _trainerService.GetGymIdByTrainer(user.Id);
+                var gym = _gymService.GetGymModel(gymId);
+
+                AttendanceChartExcelFileViewModel model = new AttendanceChartExcelFileViewModel()
+                {
+                    GymId = gymId,
+                    GymName = gym.Name
+                };
+
+                ViewBag.FileUploaded = false;
+                return View("LoadAttendanceChartData", model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                string message = "This option is not available now.";
+                return View("CustomError", message);
+            }
+        }
+
+
+        public IActionResult DownloadAttendanceChartBlank()
+        {
+            string relativePath = Resources.Resources.AttendanceChartBlankPath;
+            string absPath = Environment.CurrentDirectory + relativePath;
+
+            if (System.IO.File.Exists(absPath))
+            {
+                var fileStream = System.IO.File.OpenRead(absPath);
+                var contentType = "application/vnd.ms-excel";
+                var fileName = Path.GetFileName(absPath);
+
+                return File(fileStream, contentType, fileName);
+            }
+
+            _logger.LogError($"File {Path.GetFileName(absPath)} is not found by address {absPath}");
+            string message = "This option is not available now. Please, try again later.";
+            return View("CustomError", message);
+        }
+        
+
+        [HttpPost]
+        [Authorize(Roles = "gymAdmin")]
+        public async Task<IActionResult> LoadAttendanceChartFile(AttendanceChartExcelFileViewModel model)
+        {
+            try
+            {
+                if (model.AttendanceChartFile != null && Path.GetExtension(model.AttendanceChartFile.FileName) == ".xlsx")
+                {
+                    long length = model.AttendanceChartFile.Length;
+                    byte[] buffer = new byte[length];
+                    using (var fileStream = model.AttendanceChartFile.OpenReadStream())
+                    {
+                        await fileStream.ReadAsync(buffer, 0, (int)model.AttendanceChartFile.Length);
+                    }
+
+                    var output = await _fileService.ReadAttendanceChartFromExcelAsync(buffer, model.GymId);
+                    output = output.OrderBy(x => x.DayOfWeek).ToList();
+                    foreach (var attendanceChartModel in output)
+                    {
+                        attendanceChartModel.GymId = model.GymId;
+                        attendanceChartModel.NumberOfVisitorsPerHour = attendanceChartModel.NumberOfVisitorsPerHour.OrderBy(x => x.Hour).ToList();
+                    }
+
+                    _gymService.AddVisitingChartDataToDb(output);
+                    ViewBag.FileUploaded = true;
+                }
+                else
+                {
+                    ModelState.AddModelError("File incorrect", "Please, add the file .xlsx extension");
+                    ViewBag.FileUploaded = false;
+                }
+
+                return View("LoadAttendanceChartData", model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                string message = "There was a problem with loading the file. Please, try again later";
+
+                return View("CustomError", message);
+            }
         }
     }
 }
--- FitMeApp/Controllers/GymsController.cs
+++ FitMeApp/Controllers/GymsController.cs
@@ -2,47 +2,27 @@
 using FitMeApp.Mapper;
 using FitMeApp.Services.Contracts.Interfaces;
 using FitMeApp.WEB.Contracts.ViewModels;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using FitMeApp.Models.ExcelModels;
-using System.Resources;
-using FitMeApp.Resources;
-using FitMeApp.Services.Contracts.Models;
-using Microsoft.Extensions.Localization;
+
 
 namespace FitMeApp.Controllers
 {
     public sealed class GymsController : Controller
     {
         private readonly IGymService _gymService;
-        private readonly ITrainerService _trainerService;
         private readonly ITrainingService _trainingService;
-        private readonly IFileService _fileService;
         private readonly ModelViewModelMapper _mapper;
-        private readonly UserManager<User> _userManager;
         private readonly ILogger _logger;
 
-        public GymsController(IGymService gymService,
-            ITrainerService trainerService,
-            ITrainingService trainingService,
-            IFileService fileService,
-            UserManager<User> userManager,
-            ILogger<GymsController> logger)
+        public GymsController(IGymService gymService, ITrainingService trainingService, ILogger<GymsController> logger)
         {
             _gymService = gymService;
-            _trainerService = trainerService;
             _trainingService = trainingService;
-            _fileService = fileService;
             _mapper = new ModelViewModelMapper();
-            _userManager = userManager;
             _logger = logger;
         }
 
@@ -74,7 +54,6 @@ namespace FitMeApp.Controllers
                 string message = "Gym page is not available now.";
                 return View("CustomError", message);
             }
-
         }
 
 
@@ -114,6 +93,7 @@ namespace FitMeApp.Controllers
             }
         }
 
+
         public IActionResult CurrentGymInfo(int gymId)
         {
             try
@@ -143,104 +123,6 @@ namespace FitMeApp.Controllers
                 string message = "Gym page is not available now.";
                 return View("CustomError", message);
             }
-            
         }
-
-
-
-        //Gym settings
-
-        [Authorize(Roles = "gymAdmin")]
-        public async Task<IActionResult> LoadAttendanceChartData()
-        {
-            try
-            {
-                var user = await _userManager.GetUserAsync(User);
-                int gymId = _trainerService.GetGymIdByTrainer(user.Id);
-                var gym = _gymService.GetGymModel(gymId);
-
-                AttendanceChartExcelFileViewModel model = new AttendanceChartExcelFileViewModel()
-                {
-                    GymId = gymId,
-                    GymName = gym.Name
-                };
-
-                ViewBag.FileUploaded = false;
-                return View("LoadAttendanceChartData", model);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, ex.Message);
-                string message = "This option is not available now.";
-                return View("CustomError", message);
-            }
-        }
-
-
-        public IActionResult DownloadAttendanceChartBlank() //todo move logic to another Controller
-        {
-            string relativePath = Resources.Resources.AttendanceChartBlankPath;
-            string absPath = Environment.CurrentDirectory + relativePath;
-
-            if (System.IO.File.Exists(absPath))
-            {
-                var fileStream = System.IO.File.OpenRead(absPath);
-                var contentType = "application/vnd.ms-excel";
-                var fileName = Path.GetFileName(absPath);
-
-                return File(fileStream, contentType, fileName);
-            }
-
-            _logger.LogError($"File {Path.GetFileName(absPath)} is not found by address {absPath}");
-            string message = "This option is not available now. Please, try again later.";
-            return View("CustomError", message);
-        }
-
-
-
-        [HttpPost]
-        [Authorize(Roles = "gymAdmin")]
-        public async Task<IActionResult> LoadAttendanceChartFile(AttendanceChartExcelFileViewModel model)
-        {
-            try
-            {
-                if (model.AttendanceChartFile != null && Path.GetExtension(model.AttendanceChartFile.FileName) == ".xlsx")
-                {
-                    long length = model.AttendanceChartFile.Length;
-                    byte[] buffer = new byte[length];
-                    using (var fileStream = model.AttendanceChartFile.OpenReadStream())
-                    {
-                        await fileStream.ReadAsync(buffer, 0, (int)model.AttendanceChartFile.Length);
-                    }
-
-                    var output = await _fileService.ReadAttendanceChartFromExcelAsync(buffer, model.GymId);
-                    output = output.OrderBy(x => x.DayOfWeek).ToList();
-                    foreach (var attendanceChartModel in output)
-                    {
-                        attendanceChartModel.GymId = model.GymId;
-                        attendanceChartModel.NumberOfVisitorsPerHour = attendanceChartModel.NumberOfVisitorsPerHour.OrderBy(x => x.Hour).ToList();
-                    }
-
-                    _gymService.AddVisitingChartDataToDb(output);
-                    ViewBag.FileUploaded = true;
-                }
-                else
-                {
-                    ModelState.AddModelError("File incorrect", "Please, add the file .xlsx extension");
-                    ViewBag.FileUploaded = false;
-                }
-
-                return View("LoadAttendanceChartData", model);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, ex.Message);
-                string message = "There was a problem with loading the file. Please, try again later";
-
-                return View("CustomError", message);
-            }
-
-        }
-
     }
 }
